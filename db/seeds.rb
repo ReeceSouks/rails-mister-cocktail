75a5e11d45e696131b6aedfec748b41d34239b06
reece soukoroff <eceer.soukoroff832@gmail.com>
2019-10-25 12:39:59 +0800

    some images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "\nCreating 20 ingredients...\n"
+
+INGREDIENTS_URL = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'.freeze
+ingredients_json = open(INGREDIENTS_URL).read
+DESCRIPTIONS = %w[5cl 2cl 2l 5tbsp 10tn 420g].freeze
+
+def five_doses
+  5.times do
+    Dose.create(description: descriptions.sample, cocktail: Cocktail, ingredient: Ingredient.all.sample)
+  end
+end
+
+ingredients = JSON.parse(ingredients_json)
+random_ingredient = ingredients['drinks'].take(20)
+random_ingredient.each do |ing|
+  Ingredient.create(name: ing['strIngredient1'])
+
+end
+
+puts '==========================='
+puts '𝌏 All Ingredients Created𝌏'
+puts "===========================\n\n\n"
+
+require 'faker'
+
+puts "Generating 20 random cocktail names\n"
+20.times do
+  name = "#{Faker::Color.color_name.capitalize} #{Faker::Ancient.god} "
+  new_cocktail = Cocktail.create(name: name)
+  5.times do
+  Dose.create(description: DESCRIPTIONS.sample, cocktail: new_cocktail, ingredient: Ingredient.all.sample)
+  end
+end
+
+puts '========================='
+puts '𝌏 All Cocktails Created𝌏'
+puts "==========================\n\n\n"
+
